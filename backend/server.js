c7305d85e201bfc77ad314003b15e0cd3911e11c
Kashif Maan <kashifmaan@gmail.com>
2026-05-16 13:17:54 +0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,39 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+// CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+
+// Middleware
 app.use(express.json());
 
+
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
+
+// Server
 app.listen(process.env.PORT, () => {
   console.log(`Server Running on port ${process.env.PORT}`);
 });
